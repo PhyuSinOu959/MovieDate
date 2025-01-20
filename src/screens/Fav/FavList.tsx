a164fdb2e53cecacf911f275e4c2caccc43fc691
phyusinou <phyusinou129@gmail.com>
2025-01-20 16:28:59 +0630

    commit for adding screens

---
+++ src/screens/Fav/FavList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const FavListScreen = () => {
+  return (
+    <View>
+      <Text>Fav List Screen</Text>
+    </View>
+  );
+};
+
+export default FavListScreen;
--- src/screens/Home/featureComponent/MovieDetail/MovieDetail.tsx
+++ src/screens/Home/featureComponent/MovieDetail/MovieDetail.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 const MovieDetailScreen = () => {
   return (
-    <View>
-      <Text>Movie Detail Screen</Text>
+    <View style={styles.container}>
+      <Image source={{ uri: 'https://example.com/movie.jpg' }} style={styles.movieImage} />
+      <Text style={styles.movieRating}>Rating: 8.5/10</Text>
+      <Text style={styles.movieSummary}>This is a detailed summary of the movie. It includes the plot, characters, and other interesting details.</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    alignItems: 'center',
+  },
+  movieImage: {
+    width: 200,
+    height: 300,
+    marginBottom: 16,
+  },
+  movieRating: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  movieSummary: {
+    fontSize: 16,
+    textAlign: 'center',
+  },
+});
 
 export default MovieDetailScreen;
